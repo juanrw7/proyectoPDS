091d6613a9e333e5f332fcf08e1946ad47ee9ddc
juanrw <juan.rw24@gmail.com>
2024-10-23 00:25:21 -0500

    commit inicial, plantilla guigenie y clase usuario empezada

---
+++ Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+  // Instance variables
+  private int edad;
+  private double peso;
+  private double altura;
+  private String genero;
+
+
+  public Usuario(int edad, double peso, double altura, String genero) {
+    this.edad = edad;
+    this.peso = peso;
+    this.altura = altura;
+    this.genero = genero;
+  }
+
+  
+
+}
